89c992d2d7bf9bbcbf4774ba999804e02bd4d333
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-09-15 09:47:16 -0500

    feat: hangman

--- hangman.py
+++ hangman.py
@@ -15,3 +15,22 @@ def play(word):
     print(word_completion)
     print("\n")
     while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good Job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
