5179078b3157f94ae029142697c091a808a551cf
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-09-16 19:20:36 -0500

    Feat: blackjack

--- blackJack.py
+++ blackJack.py
@@ -47,3 +47,7 @@
 #             self.deck[card_to_swap] = self.deck[index]
 #             self.deck[index] = temp_card
 #             maximum = maximum - 1
+import os
+import random
+
+deck = [2,3,4,5,6,7,8,9,10,11,12,13,14]*4
